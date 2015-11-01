8e67d348cccbc542d74ad250376f6ad539765a4f
misuher <cm.castillo87@gmail.com>
2015-11-01 12:13:22 +0100

    create map centered in canary islands

--- main.go
+++ main.go
@@ -1,25 +1,21 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"strings"
-)
+import "net/http"
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static/"))) //working page
-	http.HandleFunc("/coordinates", coordinates)                     //ajax url
+	//http.HandleFunc("/coordinates", coordinates)                     //ajax url
 	http.ListenAndServe(":4000", nil)
 }
 
-func coordinates(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.NotFound(w, r)
-		return
-	}
-
-	field := r.FormValue("textfield")
-	mark := markdown.NewParser(strings.NewReader(field))
-	log.Println("Ajax:", field)
-	w.Write([]byte(mark.Markdown()))
-}
+// func coordinates(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method != "POST" {
+// 		http.NotFound(w, r)
+// 		return
+// 	}
+//
+// 	field := r.FormValue("textfield")
+// 	mark := markdown.NewParser(strings.NewReader(field))
+// 	log.Println("Ajax:", field)
+// 	w.Write([]byte(mark.Markdown()))
+// }
