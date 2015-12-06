823bc4a2df71b051cbe80694958b9894bb6770b1
misuher <cm.castillo87@gmail.com>
2015-12-06 12:49:30 +0000

    get hours bug fixed

--- parser/parser.go
+++ parser/parser.go
@@ -15,6 +15,7 @@ func ParsePDF(body []byte, env models.Datastore) error {
 	buffer := bytes.NewReader(body)
 	scanner := bufio.NewScanner(buffer)
 
+	var err error
 	var f models.Flight
 	for scanner.Scan() {
 		if strings.HasPrefix(scanner.Text(), "EC") {
@@ -27,13 +28,22 @@ func ParsePDF(body []byte, env models.Datastore) error {
 			f.DepPlace = scanner.Text()
 			scanner.Scan()
 			scanner.Scan()
-			f.DepTime, _ = time.Parse(time.Kitchen, scanner.Text())
+			f.DepTime, err = time.Parse("15:04", scanner.Text())
+			if err != nil {
+				log.Println("ParsePDF error:time.Parse")
+				log.Println(err)
+				return err
+			}
 			scanner.Scan()
 			scanner.Scan()
 			f.ArrPlace = scanner.Text()
 			scanner.Scan()
 			scanner.Scan()
-			f.ArrTime, _ = time.Parse(time.Kitchen, scanner.Text())
+			f.ArrTime, err = time.Parse("15:04", scanner.Text())
+			if err != nil {
+				log.Println("ParsePDF error:time.Parse")
+				return err
+			}
 			scanner.Scan()
 			scanner.Scan()
 			f.Pasajeros = scanner.Text()
@@ -47,7 +57,7 @@ func ParsePDF(body []byte, env models.Datastore) error {
 			log.Printf("%v\n", f)
 
 			date, _ := time.Parse("2006-01-02", time.Now().Format("2006-01-02"))
-			err := env.SetDaily(date, f)
+			err = env.SetDaily(date, f)
 
 			if err != nil {
 				log.Println("SetDaily error")
